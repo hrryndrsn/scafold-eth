f55d2a5bee20140cddc5689830785fa5e9042cf9
Harry Anderson <harryanderson9@gmail.com>
2022-01-26 03:14:23 +0000

    init commit

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -6,22 +6,20 @@ import "hardhat/console.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
+    // event SetPurpose(address sender, string greeting);
+    string public greeting = "Building Unstoppable Apps!!!";
 
-  // event SetPurpose(address sender, string purpose);
-
-  string public purpose = "Building Unstoppable Apps!!!";
-
-  constructor() payable {
+    constructor() payable {
     // what should we do on deploy?
-  }
+    }
 
-  function setPurpose(string memory newPurpose) public {
-      purpose = newPurpose;
-      console.log(msg.sender,"set purpose to",purpose);
-      // emit SetPurpose(msg.sender, purpose);
-  }
+    function setGreeting(string memory newPurpose) public {
+      greeting = newPurpose;
+      console.log(msg.sender,"set greeting to",greeting);
+      // emit SetPurpose(msg.sender, greeting);
+    }
 
-  // to support receiving ETH by default
-  receive() external payable {}
-  fallback() external payable {}
+    // to support receiving ETH by default
+    receive() external payable {}
+    fallback() external payable {}
 }
