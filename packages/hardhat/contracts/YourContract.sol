8854090cc7186738d9e708c2b652af1bd3c4e136
Harry Anderson <harryanderson9@gmail.com>
2022-01-26 04:01:44 +0000

    constants and immutables

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -6,25 +6,22 @@ import "hardhat/console.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
-    // State variables are stored on the blockchain
-    string public text = "Zord";
-    uint public num = 123;
+    // Constants
+    address public constant MY_ADDRESS = 0x777788889999AaAAbBbbCcccddDdeeeEfFFfCcCc;
+    uint public constant MY_UINT = 123;
 
-    function doSomething() public {
-        // local variables are not stored on the blocked chain
-        uint i = 456;
-        
-        // here are some global variables 
-        uint timestamp = block.timestamp; // the current timestamp
-        address sender = msg.sender; // address of the caller
+    //immuatables are constants which can be set inside the constructor
+    address public immutable MY_IMMUT_ADDRESS;
+    uint public immutable MY_IMMUT_UINT;
+
+    constructor(uint _uint) {
+        MY_IMMUT_UINT = _uint;
+        MY_IMMUT_ADDRESS = msg.sender;
+    // what should we do on deploy?
     }
-
     // event SetPurpose(address sender, string greeting);
     string public greeting = "Building Unstoppable Apps!!!";
 
-    constructor() payable {
-    // what should we do on deploy?
-    }
 
     function setGreeting(string memory newPurpose) public {
       greeting = newPurpose;
