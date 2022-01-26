349b292b2fde30f297efb0bea287fe99ec0dcb8e
Harry Anderson <harryanderson9@gmail.com>
2022-01-26 03:27:25 +0000

    primative types

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -6,6 +6,35 @@ import "hardhat/console.sol";
 // https://github.com/OpenZeppelin/openzeppelin-contracts/blob/master/contracts/access/Ownable.sol
 
 contract YourContract {
+    //bool
+    bool public boo = true;
+
+    //uint 
+    uint8 public u8 = 1;
+    uint16 public u256 = 456;
+    uint public u = 123; // uint is an alias for uint256
+
+    int8 public i8 = -1;
+    int16 public i16 = -525;
+    int public i = -123;
+
+    //min and max
+    int public minInt = type(int).min;
+    int public maxInt = type(int).max;
+
+    //addr 
+    address public addr = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
+
+    //bytes
+    bytes1 a = 0xb5;
+    bytes1 b = 0x65;
+    
+    //default vars
+    bool public defaltBoo; // false
+    uint public defaultUint; //0
+    int public defaultInt; //0
+    address public defaultAddr; //0x000000000000000000000000000000;
+
     // event SetPurpose(address sender, string greeting);
     string public greeting = "Building Unstoppable Apps!!!";
 
